56bd1a9e27875ecbcace5bb8d6924bfd47c9210f
Zane Ashby <zane.a@demonastery.org>
2014-09-17 10:20:40 +1200

    Add initial version of extension.

---
+++ app/code/community/Springtimesoft/Raygun/Helper/Data.php
@@ -0,0 +1,148 @@
+<?php
+class Springtimesoft_Raygun_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    const CONFIG_API_KEY = 'springtimesoft_raygun/springtimesoft_raygun_group/springtimesoft_raygun_api_key';
+    const CONFIG_ASYNC = 'springtimesoft_raygun/springtimesoft_raygun_advanced_group/springtimesoft_raygun_async';
+    const CONFIG_TAGS = 'springtimesoft_raygun/springtimesoft_raygun_advanced_group/springtimesoft_raygun_tags';
+    const CONFIG_ENABLED = 'springtimesoft_raygun/springtimesoft_raygun_group/springtimesoft_raygun_enabled';
+
+    protected $client = null;
+
+    /**
+     * Return Magento version.
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return Mage::getVersion();
+    }
+
+    /**
+     * Return the Raygun API Key.
+     *
+     * @return string
+     */
+    public function getAPIKey()
+    {
+        return (string)Mage::getStoreConfig(self::CONFIG_API_KEY);
+    }
+
+    /**
+     * True if Raygun should be used asynchronously.
+     *
+     * @return bool
+     */
+    public function getAsync()
+    {
+        return (bool)Mage::getStoreConfig(self::CONFIG_ASYNC);
+    }
+
+    /**
+     * Return tags to append to errors.
+     * 
+     * @return array
+     */
+    public function getTags()
+    {
+        return explode(' ', (string)Mage::getStoreConfig(self::CONFIG_TAGS));
+    }
+
+    /**
+     * True if Raygun is enabled.
+     * 
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return (bool)Mage::getStoreConfig(self::CONFIG_ENABLED);
+    }
+
+    /**
+     * Return the Raygun client.
+     * 
+     * @return RaygunClient
+     */
+    public function getClient()
+    {
+        if (!$this->getAPIKey() || !$this->isEnabled())
+            return null;
+
+        if (!$this->client) {
+            require_once __DIR__ . '/../Raygun4php/RaygunClient.php'; 
+
+            $this->client = new \Raygun4php\RaygunClient($this->getAPIKey(), $this->getAsync(), false);
+            $this->client->SetVersion($this->getVersion());
+
+            $customer = Mage::helper('customer')->getCustomer();
+
+            if ($customer !== null) {
+                $this->client->SetUser(
+                    $customer->getEmail(),           // Unique ID
+                    $customer->getFirstname(),       // First name
+                    $customer->getName(),            // Full name
+                    $customer->getEmail(),           // Email
+                    ($customer->getEmail() == null), // Anonymous?
+                    null                             // UUID
+                );
+            }
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * Shutdown error handler.
+     *
+     * This can be a bit problematic so don't do this for now.
+     */
+    public function shutdownHandler()
+    {
+        /*
+        $err = error_get_last();
+
+        if (isset($err['type']) && $err['type'] !== E_WARNING) {
+            //$this->exceptionHandler(new ErrorException($err['type'], $err['message'], $err['file'], $err['line']));
+        }
+        */
+    }
+
+    /**
+     * Error handler.
+     * 
+     * @param  int    $errno   Error number
+     * @param  string $errstr  Error description
+     * @param  string $errfile File the error occured in
+     * @param  int    $errline Line the error occured on
+     */
+    public function errorHandler($errno, $errstr, $errfile, $errline)
+    {
+        // Ignore empty error messages
+        if ($errno == null && $errstr == null)
+            return;
+
+        // Ignore intentionally silenced errors (i.e. using the '@' operator)
+        // When using this PHP temporarily sets the error_reporting setting to 0
+        // so we can check this way.
+        if (error_reporting() === 0)
+            return;
+
+        // Wrap in an ErrorException and pass off to exception handler.
+        $this->exceptionHandler(new ErrorException($errstr, 0, $errno, $errfile, $errline));
+    }
+
+    /**
+     * Exception error handler.
+     * This takes care of actually submitting the message to Raygun (via the client API).
+     *
+     * @param  Exception $exception PHP Exception
+     */
+    public function exceptionHandler($exception)
+    {
+        if (!$this->getClient())
+            return;
+        
+        // Send the exception!
+        return $this->getClient()->SendException($exception, $this->getTags());
+    }
+}
---
+++ app/code/community/Springtimesoft/Raygun/Model/Observer.php
@@ -0,0 +1,35 @@
+<?php
+class Springtimesoft_Raygun_Model_Observer
+{
+    /**
+     * Register error handler earlish in Magento boot.
+     * 
+     * @param  Varien_Event_Observer $event [description]
+     */
+    public function registerHandlers(Varien_Event_Observer $event)
+    {
+        set_error_handler('Springtimesoft_Raygun_Model_Observer::errorHandler');
+
+        // Maybe one day we can use these...
+        //set_exception_handler('Springtimesoft_Raygun_Model_Observer::exceptionHandler');
+        //register_shutdown_function('Springtimesoft_Raygun_Model_Observer::shutdownHandler');
+    }
+
+    /**
+     * Catch errors and pass them off to helper.
+     */
+    public static function errorHandler($errno = null, $errstr = null, $errfile = null, $errline = null)
+    {
+        Mage::helper('springtimesoft_raygun')->errorHandler($errno, $errstr, $errfile, $errline);
+    }
+
+    public static function exceptionHandler($exception)
+    {
+        Mage::helper('springtimesoft_raygun')->exceptionHandler($exception);
+    }
+
+    public static function shutdownHandler()
+    {
+        Mage::helper('springtimesoft_raygun')->shutdownHandler();
+    }
+}
